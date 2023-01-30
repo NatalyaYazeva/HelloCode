1e72097ccd04aafc6b7ace5d4e9ac2007c70576d
Natalya <yazeva_natalya@mail.ru>
2023-01-30 20:50:00 +0300

    10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1,2,3,9,5,8,7,9};
+
+int n = array.Length;
+int find =9;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
