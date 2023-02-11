36e4029b86ce9c854eede6ee9049635f33c294f6
Natalya <yazeva_natalya@mail.ru>
2023-02-11 21:37:50 +0300

    add lesson dIRECTORY

--- Example015_Recursion2/Program.cs
+++ Example015_Recursion2/Program.cs
@@ -231,7 +231,7 @@ Console.WriteLine();
 Console.ReadLine();*/
 
 // закрашивание картинки
-int[,] pic = new int[10,10];
+/*int[,] pic = new int[10,10];
 for (int i = 0; i < 10; i++)
 {
     pic[0,i] = 1;
@@ -268,4 +268,37 @@ void FillImage (int row, int col)
         FillImage(row + 1, col);
         FillImage(row, col + 1);
     }
+}*/
+
+//-------------DIRECTORY----------------------
+
+/*string path = "D:/GB/Языки программирования/Examples/Example016_Directory";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}*/
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
 }
+
+string path = @"D:/GB/Языки программирования/Examples/Example015_Recursion2";
+CatalogInfo(path);
